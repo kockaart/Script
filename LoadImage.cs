2a807b0d4984262dcb635ee7d360f22ce0ba6017
kockaart <andras.koczka@gmail.com>
2014-05-22 12:41:38 +0200

    load from txt

--- LoadImage.cs
+++ LoadImage.cs
@@ -6,10 +6,24 @@ public class LoadImage : MonoBehaviour {
 	// and DXT compress them at runtime
 	public string  url = "file://localhost/"+Application.dataPath+"/../photo.png";
 	public Texture2D photo;
+	
+	//public TextAsset txt;
+	public string name= "Please set name";
 
 	// Update is called once per frame
 	public IEnumerator Start () {
 		// Create a texture in DXT1 format
+		//Texture texture_title=Resources.Load("title5") as Texture;
+		//GameObject.Find("title").renderer.material.mainTexture=texture_title;
+		//kell a Resources konyvtar az assetben
+
+		//1* txt = (TextAsset)Resources.Load("name", typeof(TextAsset));
+		//2* txt.Load();
+		//name = txt.text;
+		
+		sr = new StreamReader(Application.dataPath + "/../name.txt"); 
+		name = sr.ReadLine();
+		
 		photo = new Texture2D(4, 4, TextureFormat.DXT1, false);
 		renderer.material.mainTexture = photo;
 		while(true) {
@@ -20,6 +34,8 @@ public class LoadImage : MonoBehaviour {
 			// assign the downloaded image to the main texture of the object
 			www.LoadImageIntoTexture(photo);
 		}
+		//print(Application.persistentDataPath);
+		//string fileLocation = Application.persistentDataPath + "file.txt";
 	}
 	
 }
