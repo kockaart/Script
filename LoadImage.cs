41682896fb9e25cdf4b17d955587eb425b39ffd7
kockaart <andras.koczka@gmail.com>
2014-05-23 11:04:02 +0200

    5.23

--- LoadImage.cs
+++ LoadImage.cs
@@ -4,38 +4,30 @@ using System.Collections;
 public class LoadImage : MonoBehaviour {
 
 	// and DXT compress them at runtime
-	public string  url = "file://localhost/"+Application.dataPath+"/../photo.png";
+	public string  url = "file://localhost/"+Application.dataPath+"/../player_photo.png";
 	public Texture2D photo;
-	
+
+
 	//public TextAsset txt;
 	public string name= "Please set name";
 
 	// Update is called once per frame
-	public IEnumerator Start () {
+	void Start() {
 		// Create a texture in DXT1 format
 		//Texture texture_title=Resources.Load("title5") as Texture;
 		//GameObject.Find("title").renderer.material.mainTexture=texture_title;
-		//kell a Resources konyvtar az assetben
 
-		//1* txt = (TextAsset)Resources.Load("name", typeof(TextAsset));
-		//2* txt.Load();
-		//name = txt.text;
-		
-		sr = new StreamReader(Application.dataPath + "/../name.txt"); 
-		name = sr.ReadLine();
-		
-		photo = new Texture2D(4, 4, TextureFormat.DXT1, false);
-		renderer.material.mainTexture = photo;
-		while(true) {
-			// Start a download of the given URL
-			var www = new WWW(url);
-			// wait until the download is done
-			yield return www;
-			// assign the downloaded image to the main texture of the object
-			www.LoadImageIntoTexture(photo);
-		}
-		//print(Application.persistentDataPath);
-		//string fileLocation = Application.persistentDataPath + "file.txt";
+		Texture2D tex=Resources.Load("player_photo") as Texture2D;
+		photo = tex;
+		this.renderer.material.mainTexture=photo;
+		Resources.UnloadAsset(tex);
+
+
+		TextAsset txt = (TextAsset)Resources.Load("player_name", typeof(TextAsset));
+		//txt.Load();
+		name = txt.text;
+		Resources.UnloadAsset(txt);
+
+		print(name);
 	}
-	
 }
--- SoccerPlayer.cs
+++ SoccerPlayer.cs
@@ -1,12 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof(AudioSource))]
+
 public class SoccerPlayer : MonoBehaviour
 {
-
-    public LoadImage loadImage;
     private Color guiColor;
     public KinectManager km;
+	public LoadImage lm;
 
     public Vector3 posLeft;
     public Vector3 posRight;
@@ -22,8 +23,6 @@ public class SoccerPlayer : MonoBehaviour
     public Texture2D leftK3;
     public Texture2D leftK4;
     public Texture2D leftK5;
-    public Texture2D result;
-    public Texture2D picture;
     public MovieTexture loop;
     public MovieTexture lf;
     public MovieTexture lg;
@@ -31,8 +30,6 @@ public class SoccerPlayer : MonoBehaviour
     public MovieTexture cg;
     public MovieTexture rf;
     public MovieTexture rg;
-    public MovieTexture goal;
-    public MovieTexture fail;
     public MovieTexture shining;
 
     public MovieTexture result;
@@ -42,11 +39,14 @@ public class SoccerPlayer : MonoBehaviour
     public MovieTexture result4;
     public MovieTexture result5;
 
+	private MovieTexture movTexture;
+	private MovieTexture resTexture;
+
     public int counter = 0;
     public int counter2 = 0;
+	public int counter3 = 0;
     public int animation = 0;
     public int animationTime = 800;
-    public int time = 70;
     public int time2 = 240;
     public int kicksLeft = 5;
     public int score = 0;
@@ -79,13 +79,15 @@ public class SoccerPlayer : MonoBehaviour
     private Vector3 left4;
     private Vector3 left5;
 
-    public float sensitivity = 0.5f;
-    public float threshold = 0.5f;
+    public float sensitivity = 0.4f;
+    public float threshold = 0.2f;
 
     // Use this for initialization
     void Start()
     {
         guiColor = Color.white;
+		movTexture = lg;
+		resTexture = movTexture;
     }
 
     // Update is called once per frame
@@ -94,35 +96,6 @@ public class SoccerPlayer : MonoBehaviour
         posRight = Foot_Right.transform.localPosition;
         posLeft = Foot_Left.transform.localPosition;
         posHead = Head.transform.localPosition;
-
-        if (!lf.isPlaying) lf.Stop();
-        if (!lg.isPlaying) lg.Stop();
-        if (!rf.isPlaying) rf.Stop();
-        if (!rg.isPlaying) rg.Stop();
-        if (!cf.isPlaying) cf.Stop();
-        if (!cg.isPlaying) cg.Stop();
-        if (!result.isPlaying) result.Stop();
-        if (!result1.isPlaying) result1.Stop();
-        if (!result2.isPlaying) result.Stop();
-        if (!result3.isPlaying) result.Stop();
-        if (!result4.isPlaying) result.Stop();
-        if (!result5.isPlaying) result.Stop();
-
-        if (lf.isPlaying) play = true;
-        else if (lg.isPlaying) play = true;
-        else if (rf.isPlaying) play = true;
-        else if (rg.isPlaying) play = true;
-        else if (cf.isPlaying) play = true;
-        else if (cg.isPlaying) play = true;
-        else play = false;
-
-        if (result.isPlaying) playResult = true;
-        else if (result1.isPlaying) playResult = true;
-        else if (result2.isPlaying) playResult = true;
-        else if (result3.isPlaying) playResult = true;
-        else if (result4.isPlaying) playResult = true;
-        else if (result5.isPlaying) playResult = true;
-        else playResult = false;
 
         previous = previous1;
         previous1 = previous2;
@@ -146,197 +119,191 @@ public class SoccerPlayer : MonoBehaviour
         left4 = left5;
         left5 = Foot_Left.transform.localPosition;
 
-
         //		if (Input.anyKey){
         //			Debug.Log("A key or mouse click has been detected");
         //
         //		}
     }
 
-    public GUISkin menuSkin;
     public GUISkin menuSkin2;
     public GUISkin menuSkin3;
     public GUISkin menuSkin4;
-    public string kicks = "KICKS REMAINING: ";
     public string name = "ALEXANDR!";
 
     void OnGUI()
     {
-        if (!lf.isPlaying) lf.Stop();
-        if (!lg.isPlaying) lg.Stop();
-        if (!rf.isPlaying) rf.Stop();
-        if (!rg.isPlaying) rg.Stop();
-        if (!cf.isPlaying) cf.Stop();
-        if (!cg.isPlaying) cg.Stop();
-        if (!result.isPlaying) result.Stop();
-        if (!result1.isPlaying) result1.Stop();
-        if (!result2.isPlaying) result.Stop();
-        if (!result3.isPlaying) result.Stop();
-        if (!result4.isPlaying) result.Stop();
-        if (!result5.isPlaying) result.Stop();
+		if (!movTexture.isPlaying){
+			if (!lf.isPlaying) lf.Stop();
+			if (!lg.isPlaying) lg.Stop();
+			if (!rf.isPlaying) rf.Stop();
+			if (!rg.isPlaying) rg.Stop();
+			if (!cf.isPlaying) cf.Stop();
+			if (!cg.isPlaying) cg.Stop();
+			if (!result.isPlaying){
+				result.Stop();
+				renderer.enabled=false;
+			}
+			if (!result1.isPlaying){
+				result1.Stop();
+				renderer.enabled=false;
+			}
+			if (!result2.isPlaying){
+				result.Stop();
+				renderer.enabled=false;
+			}
+			if (!result3.isPlaying){
+				result.Stop();
+				renderer.enabled=false;
+			}
+			if (!result4.isPlaying){
+				result.Stop();				
+				renderer.enabled=false;
+			}
+			if (!result5.isPlaying){ 
+				result.Stop();
+				renderer.enabled=false;
+			}
+		}
+		
+		if (movTexture.isPlaying){
+			play = true;
+			counter3++;
+		}
+		else{
+			play = false;
+			counter3=0;
+		}
 
-        if (lf.isPlaying) play = true;
-        else if (lg.isPlaying) play = true;
-        else if (rf.isPlaying) play = true;
-        else if (rg.isPlaying) play = true;
-        else if (cf.isPlaying) play = true;
-        else if (cg.isPlaying) play = true;
-        else play = false;
-
-        if (result.isPlaying) playResult = true;
-        else if (result1.isPlaying) playResult = true;
-        else if (result2.isPlaying) playResult = true;
-        else if (result3.isPlaying) playResult = true;
-        else if (result4.isPlaying) playResult = true;
-        else if (result5.isPlaying) playResult = true;
-        else playResult = false;
+		if (resTexture.isPlaying){
+			playResult = true;
+			movTexture.Play();
+		}
+		else if (result.isPlaying) playResult = true;
+		else if (result1.isPlaying) playResult = true;
+		else if (result2.isPlaying) playResult = true;
+		else if (result3.isPlaying) playResult = true;
+		else if (result4.isPlaying) playResult = true;
+		else if (result5.isPlaying) playResult = true;
+		else playResult = false;
 
         GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), loop);
         loop.Play();
         loop.loop = true;
-        if (play)
-        {
-            if (random == 0)
-            {
-                GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), lf);
-                lf.Play();
-            }
-            if (random == 1)
-            {
-                GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), lg);
-                lg.Play();
-            }
-            if (random == 2)
-            {
-                GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), rf);
-                rf.Play();
-            }
-            if (random == 3)
-            {
-                GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), rg);
-                rg.Play();
-            }
-            if (random == 4)
-            {
-                GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), cf);
-                cf.Play();
-            }
-            if (random == 5)
-            {
-                GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), cg);
-                cg.Play();
-            }
 
+        if (play && counter3>5)
+        {
+			GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), movTexture);
+			movTexture.Play();
         }
+
         //player present
         if (posRight.z != 0 && previous != Head.transform.localPosition.z)
         {
             approach = false;
             if (kicksLeft == 0 && counter2 < time2 && counter2 > 0 && !play) counter2++;
             if (kicksLeft > 0 && !play && animation > animationTime)
-            {
+            {		
+				//print (Mathf.Abs((right5.x) - (right.x)));
                 counter++;
-                plane.active = true;
+                //plane.active = true;
+				plane.GetComponent<LoadImage>().enabled = true;
                 //right leg
                 if (counter > 8 && (right5.z - right.z) > sensitivity )
                 {
-                    random = Random.Range(0, 1);
+					random = Random.Range(0, 2);
                     //failed
                     if (random == 0)
                     {
-                        if ((Math.Abs(right5.x) - Math.Abs(right.x)) < threshold )
+						if (Mathf.Abs((right5.x) - (right.x)) < threshold )
                         {
-                            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), cf);
-                            cf.Play();
+							cf.Stop();
+							movTexture=cf;                      
                         }
                         else if (right5.x > right.x)
                         {
-                            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), rf);
-                            rf.Play();
+							rf.Stop();
+							movTexture=rf;                           
                         }
                         else {
-                            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), lf);
-                            lf.Play();
+							lf.Stop();
+							movTexture=lf;                            
                         }
                     }
                     //goal
-                    if (random == 1)
+					if (random == 1 || random == 2)
                     {
-                        if ((Math.Abs(right5.x) - Math.Abs(right.x)) < threshold)
+						if (Mathf.Abs((right5.x) - (right.x)) < threshold)
                         {
-                            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), cg);
-                            cg.Play();
+							cg.Stop();
+							movTexture=cg;
                         }
-                        else if (right5.x > right.x)
-                        {
-                            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), rg);
-                            rg.Play();
-                        }
+						else if (right5.x > right.x)
+						{
+							rg.Stop();
+							movTexture=rg;                           
+						}
                         else
                         {
-                            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), lg);
-                            lg.Play();
+							lg.Stop();
+							movTexture=lg;
                         }
                         score++;
                     }
                     counter = 0;
                     kicksLeft--;
+					movTexture.Play();
                 }
                 //leftleg
                 if (counter > 8 && (left5.z - left.z) > sensitivity)
                 {
-                    random = Random.Range(0, 1);
-                    //failed
-                    if (random == 0)
-                    {
-                        if ((Math.Abs(left5.x) - Math.Abs(left.x)) < threshold)
-                        {
-                            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), cf);
-                            cf.Play();
-                        }
-                        else if (left5.x > left.x)
-                        {
-                            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), rf);
-                            rf.Play();
-                        }
-                        else
-                        {
-                            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), lf);
-                            lf.Play();
-                        }
-                    }
-                    //goal
-                    if (random == 1)
-                    {
-                        if ((Math.Abs(left5.x) - Math.Abs(left.x)) < threshold)
-                        {
-                            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), cg);
-                            cg.Play();
-                        }
-                        else if (left5.x > left.x)
-                        {
-                            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), rg);
-                            rg.Play();
-                        }
-                        else
-                        {
-                            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), lg);
-                            lg.Play();
-                        }
-                        score++;
-                    }
-                    counter = 0;
-                    kicksLeft--;
+					random = Random.Range(0, 2);
+					//failed
+					if (random == 0)
+					{
+						if (Mathf.Abs((left5.x) - (left.x)) < threshold )
+						{
+							cf.Stop();
+							movTexture=cf;                      
+						}
+						else if (left5.x > left.x)
+						{
+							rf.Stop();
+							movTexture=rf;                           
+						}
+						else {
+							lf.Stop();
+							movTexture=lf;                            
+						}
+					}
+					//goal
+					if (random == 1 || random == 2)
+					{
+						if (Mathf.Abs((left5.x) - (left.x)) < threshold)
+						{
+							cg.Stop();
+							movTexture=cg;
+						}
+						else if (left5.x > left.x)
+						{
+							rg.Stop();
+							movTexture=rg;                           
+						}
+						else
+						{
+							lg.Stop();
+							movTexture=lg;
+						}
+						score++;
+					}
+					counter = 0;
+					kicksLeft--;
+					movTexture.Play();
                 }
             }
         }
 
-        if (counter > 0 && counter < time && kicksLeft > 0 && approach == false && animation > animationTime && !play)
+        if (counter > 0 && kicksLeft > 0 && approach == false && animation > animationTime && !play)
         {
-            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), loop);
-            loop.Play();
-            loop.loop = true;
-            //GUI.Box (new Rect (50, Screen.height-100,460,90), (kicks + kicksLeft));
             if (kicksLeft == 5)
             {
                 GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), leftK5);
@@ -358,51 +325,57 @@ public class SoccerPlayer : MonoBehaviour
                 GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), leftK1);
             }
             GUI.skin = menuSkin2;
-            GUI.Box(new Rect(Screen.width - 500, Screen.height - 170, 360, 90), name);
+            GUI.Box(new Rect(Screen.width*2/3, Screen.height*9/10, 460, 120), lm.name);
         }
 
         if (play == false && approach == false && animation > animationTime)
         {
+			print ("silhouette");
             guiColor.a = 0.28f;
             GUI.color = guiColor; //sets all next gui color to transparent 0,5
             //guiTexture.color = colorT;
             GUI.DrawTexture(new Rect(Screen.width * 5 / 40, Screen.height, Screen.width * 15 / 20, -Screen.height * 8 / 10), km.usersLblTex);
             GUI.color = Color.white; //sets it back
         }
-        if (kicksLeft == 0 && counter2 < time2 && counter2 > 0 && !play)
-        {
-          
-            renderer.material.mainTexture = result;
-            result.Play();
+        if (kicksLeft == 0 && counter2 > 0 && !play) {
+			if(score==0) movTexture=result;
+			if(score==1) movTexture=result1;
+			if(score==2) movTexture=result2;
+			if(score==3) movTexture=result3;
+			if(score==4) movTexture=result4;
+			if(score==5) movTexture=result5;
 
-            GUI.skin = menuSkin4;
-            GUI.Box(new Rect(Screen.width / 4, Screen.height * 6 / 20, 560, 90), name);
-            //GUI.Box(new Rect(Screen.width / 4, Screen.height * 8 / 20, 360, 90), (score + "/5"));
-            //GUI.DrawTexture(new Rect(Screen.width * 3 / 40, Screen.height * 4 / 20, 324, 576), picture);
-            //GUI.Box (new Rect (600, 470,360,90), "picture");
-            if (counter2 > 10)
-            {
-                GUI.skin = menuSkin3;
-                GUI.Box(new Rect(Screen.width / 4, Screen.height * 12 / 20, 650, 90), "LIKE YOUR result on FB/adress!");
-            }
-             //*screenshot comes and made here
-            if (counter2 == 10 && score > 2)
-            {
-                Application.CaptureScreenshot(no + ". " + name + " " + score + " goal.png", 4);
-                no++;
-            }
-            //
-            guiColor.a = 0.3f;
-            GUI.color = guiColor; //sets all next gui color to transparent 0,5
-            //guiTexture.color = colorT;
-            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), shining);
-            shining.Play();
-            GUI.color = Color.white; //sets it back
 
-            counter2++;
-        }
-        else plane.active = false;
+			movTexture = renderer.material.mainTexture as MovieTexture;
+			movTexture.Play();
+			renderer.enabled=true;
 
+			GUI.skin = menuSkin4;
+			GUI.Box (new Rect (Screen.width / 4, Screen.height * 5 / 20, 560, 90), lm.name);
+			if (counter2 > 10) {
+					GUI.skin = menuSkin3;
+					GUI.Box (new Rect (Screen.width / 4, Screen.height * 12 / 20, 650, 150), "LIKE YOUR result on FB/adress!");
+			}
+			//*screenshot comes and made here
+			if (counter2 == 10 && score > 2) {
+					Application.CaptureScreenshot (no + ". " + lm.name + " " + score + " goal.png", 4);
+					no++;
+			}
+			//
+			guiColor.a = 0.3f;
+			GUI.color = guiColor; //sets all next gui color to transparent 0,5
+			//guiTexture.color = colorT;
+			GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), shining);
+			shining.Play ();
+			GUI.color = Color.white; //sets it back
+
+			counter2++;
+		} else {
+				plane.GetComponent<LoadImage> ().enabled = false;
+		}
+
+
+		//restart
         if (posRight.z == 0 | previous == Head.transform.localPosition.z)
         {
             kicksLeft = 5;
